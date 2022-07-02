3b89c890cecb57077b6e51c074dcb338969d8fc2
uchu <pavel.burylichev@gmail.com>
2022-07-02 17:17:08 +0700

    Improvements to the interactive part

--- app/interact.ts
+++ app/interact.ts
@@ -8,21 +8,24 @@ import moment from "moment";
 
 export class Interact {
 
-    canPost = true;
-    waitingForProposalPreviewNumber = false;
+    cantPostChatIds: number[] = [];
+    waitingForProposalPreviewNumberChatIds: number[] = [];
+    verifiedUsers: number[] = [];
+    rateLimit = 400; // User can post not more often then this amount of time in ms.
 
-    mainMenuButtons = Markup.keyboard( [
-            { text: ICONS.informal + ' Informal' },
-            { text: ICONS.formal + ' Formal' },
-            { text: ICONS.completed + ' Completed' },
-            { text: ICONS.discussions + ' Discussions' },
-            { text: ICONS.digest + ' Digest' },
-            { text: ICONS.proposal + ' Proposal #' },
-            { text: ICONS.alert + ' My Alerts' },
-        ],
-        {
-            columns: 2,
-        },
+    mainButtons = [
+        { text: ICONS.informal + ' Informal' },
+        { text: ICONS.formal + ' Formal' },
+        { text: ICONS.completed + ' Completed' },
+        { text: ICONS.discussions + ' Discussions' },
+        { text: ICONS.digest + ' Digest' },
+        { text: ICONS.proposal + ' Proposal #' },
+        //{ text: ICONS.alert + ' My Alerts' },
+    ]
+
+    mainMenuButtons = Markup.keyboard(
+        this.mainButtons,
+        { columns: 2 },
     );
 
     alertsButtons = Markup.keyboard( [
@@ -44,6 +47,13 @@ export class Interact {
 
     async verifyUser( ctx ): Promise<boolean> {
         const userId = ( await ctx.getChat() ).id;
+
+        // Check if this user is already verified.
+        if ( this.verifiedUsers.includes( userId ) ) {
+            return true;
+        }
+
+        // Check if user is a member of the common Bot channel.
         const channelUser = await ctx.telegram.getChatMember( Number( process.env.CHAT_ID ), userId );
         if ( userId !== channelUser.user.id ) {
             await ctx.replyWithMarkdown(
@@ -51,6 +61,12 @@ export class Interact {
                 `of the DxD VAs group Helper Bot channel` );
             return false;
         }
+
+        // Add users to the temporary array, so we can avoid double checks until the next app relaunch.
+        if ( !this.verifiedUsers.includes( userId ) ) {
+            this.verifiedUsers.push( userId );
+        }
+
         // Ignore old messages in case of bot reload happened.
         if ( moment.unix( ctx.message.date ).utc().diff( moment().utc(), 'seconds' ) < -60 ) {
             logger.warn( 'Message older than 60 seconds detected, ignoring.' );
@@ -63,10 +79,7 @@ export class Interact {
         this.bot.start( async ( ctx ) => {
             if ( !await this.verifyUser( ctx ) ) return;
             ctx.reply( `Hi ${ ctx.update.message.from.first_name }` );
-            await ctx.replyWithMarkdown(
-                'Select an option:',
-                this.mainMenuButtons,
-            );
+            await this.showMainMenu( ctx );
         } );
 
         this.bot.hears( ICONS.informal + ' Informal', async ( ctx ) => {
@@ -134,7 +147,8 @@ export class Interact {
         this.bot.hears( ICONS.proposal + ' Proposal #', async ( ctx ) => {
             if ( !await this.verifyUser( ctx ) ) return;
             await ctx.reply( 'Enter the proposal number for the preview:' );
-            this.waitingForProposalPreviewNumber = true;
+            this.reset( ctx );
+            this.waitingForProposalPreviewNumberChatIds.push( ctx.chat.id );
         } );
 
         this.bot.hears( ICONS.home + ' Main Menu', async ( ctx ) => {
@@ -154,29 +168,44 @@ export class Interact {
 
         this.bot.on( 'text', async ( ctx ) => {
             if ( !await this.verifyUser( ctx ) ) return;
+
+            // Check if it's an unknown command and redraw the menu.
+            let unknownMessage = true;
+            for ( const command of this.mainButtons ) {
+                if ( command.text === ctx.message.text ) {
+                    unknownMessage = false;
+                }
+            }
+
             let proposalId: number;
-            if ( this.waitingForProposalPreviewNumber ) {
-                this.reset();
+            if ( this.waitingForProposalPreviewNumberChatIds.includes( ctx.chat.id ) ) {
+                this.reset( ctx );
                 proposalId = parseInt( ctx.message.text );
                 if ( !proposalId ) {
                     ctx.reply( 'Incorrect proposal number.' );
+                    await this.showMenu( ctx );
                     return;
                 }
             } else {
+                if ( unknownMessage ) {
+                    await this.showMenu( ctx );
+                }
                 return;
             }
 
             const url = process.env.JSON_PROPOSAL_URL + proposalId;
 
             const result = await this.digest.apiClient.get( url ).catch(
-                error => {
+                async error => {
                     logger.warn( error.toString().substring( 0, 255 ) );
-                    ctx.reply( 'Unable to find the specified proposal.' )
+                    ctx.reply( 'Unable to find the specified proposal.' );
+                    await this.showMenu( ctx );
                 }
             );
 
             if ( !result.success ) {
-                ctx.reply( 'Unable to find the specified proposal.' )
+                ctx.reply( 'Unable to find the specified proposal.' );
+                await this.showMenu( ctx );
                 return;
             }
 
@@ -187,21 +216,47 @@ export class Interact {
                 `__${ this.digest.escapeText( result.proposal.title ) }__](${ link })\n\n` +
                 `${ this.digest.escapeText( result.proposal.short_description.substring( 0, 2048 ) + '...' ) }`
             );
+            await this.showMenu( ctx );
         } );
     }
 
-    async replyOnAction( ctx: any, method: any ): Promise<void> {
-        this.reset();
-        if ( !this.canPost ) return;
-        this.canPost = false;
-        const text = await method();
-        await ctx.replyWithMarkdownV2( text );
-        setTimeout( () => {
-            this.canPost = true;
-        }, 500 );
+    async showMenu( ctx: any ): Promise<void> {
+        await this.showMainMenu( ctx );
     }
 
-    reset() {
-        this.waitingForProposalPreviewNumber = false;
+    async showMainMenu( ctx: any ): Promise<void> {
+        await ctx.replyWithMarkdown(
+            'Select an option:',
+            this.mainMenuButtons,
+        );
+    }
+
+    async replyOnAction( ctx: any, method: any ): Promise<void> {
+        this.reset( ctx );
+
+        // Prevent too often posts by temporary saving the chat id.
+        if ( this.cantPostChatIds.includes( ctx.chat.id ) ) {
+            return;
+        }
+        this.cantPostChatIds.push( ctx.chat.id );
+
+        const text = await method();
+        await ctx.replyWithMarkdownV2( text );
+
+        // Allow user to post again. Remove it from the list.
+        setTimeout( () => {
+            const index = this.cantPostChatIds.indexOf( ctx.chat.id );
+            if ( index !== -1 ) {
+                this.cantPostChatIds.splice( index, 1 );
+            }
+        }, this.rateLimit );
+    }
+
+    reset( ctx: any ) {
+        // Remove user from the list who's waiting to post the proposal id.
+        const index = this.waitingForProposalPreviewNumberChatIds.indexOf( ctx.chat.id );
+        if ( index !== -1 ) {
+            this.waitingForProposalPreviewNumberChatIds.splice( index, 1 );
+        }
     }
 }
